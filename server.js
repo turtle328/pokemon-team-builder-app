b7d751e01deded53066671dd16e0a6bdc0fb3066
Alex Rosenbach <ajr6974@rit.edu>
2021-04-19 14:43:35 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const unirest = require("unirest");
+const API_KEY = "00c205d1-5cd8-4863-b9b6-6785c2e44dd1";
+
+app.get('/api/associations/:word', (req, res) => {
+	const word = req.params.word;
+	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	.then(response => {
+		const results = response.body.response[0].items || []; // grab array of results
+		console.log(`Num results=${results.length}`);
+		res.json(results);
+	})
+	.catch(error => {
+		console.log(`error=${error}`);
+		res.json({status:"Error", message: `${error}`});
+	});
+});
+
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
+});
